0052fce5765d79a9f9a7de8c6b6a19e538157790
Jack Fiacco <123217007+JackDFiacco6@users.noreply.github.com>
2023-01-27 18:19:01 -0500

    Uploading all files

---
+++ Underscore.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CIS 231 Underscore.js Demonstration</title>
+    <meta charset="utf-8" />
+    <meta name="author" content="jack"/>
+    <meta name="date" content="DATE"/>
+    <meta name="description" content="A TEMPLATE FOR HTML"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <script type = "text/javascript" script src="js/underscore-min.js"><script>
+    function main () {
+      var list = [];
+      for (let num=1; num<=100; num++) {
+        list.push
+      }
+      var shuffled = list;
+      // shuffled[2]=9999;
+      shuffled = _.shuffle(shuffled);
+      console.log(list) ;
+      console.log(shuffled) ;
+    }
+    
+    
+
+    </>
+  </head>
+
+
+
+  <body>
+<h1>header</h1>
+<input type="number"id="guessBox">
+<button type="button"id="guessButton" onclick="" ></button>
+
+
+    
+  </body>
+</html>
+
+
+        
+
+        
+
+    
+
+
+
+
+
